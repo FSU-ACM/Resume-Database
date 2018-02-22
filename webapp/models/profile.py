1c39f3f744969a5e6e72e07a413c96aeec6b10ec
Logan <ljanderson123@comcast.net>
2018-02-22 12:26:36 -0500

    Furthest i could get

--- webapp/models/profile.py
+++ webapp/models/profile.py
@@ -1,20 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-# Create your models here.
+class Profile(AbstractUser):
+	is_student = models.BooleanField(defualt=False)
+	is_companyrep = models.BoolenaField(default=False)
+#	is_admin = models.BooleanField(default=False)
 
-class Profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	email = models.TextField(max_length=100, blank=True)
-	password = models.CharField(max_length=32, blank=True)
+class Student(models.Model):
+	user = models.OneToOneField(Profile, on_delete=models.CASCADE, primary_key=True)
+	resume = models.ManyToManyField(Resume, through='ResumeSubmitted')
+	cover_letter = models.ManyToManyField(Cover_Letter, through='CoverLetterSumbitted)
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-	if created:
-	 	Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-	instance.profile.save()
